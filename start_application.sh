c1d2e500e92a4c766656889016bad13f3961a385
Kakalasek <example@email.com>
2025-03-23 20:40:34 +0100

    a lot of changes

--- start_application.sh
+++ start_application.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+cd src
+
 sudo ipfixprobe -i 'raw;ifc=ens3' -p "pstats" -p "tls" -o "unirec;i=u:mysocket:timeout=WAIT;p=(pstats,tls)" &
 /usr/bin/nemea/unirecfilter -F 'port == 443' -i u:mysocket,u:filtered &
-./module/flowclassifier.py -i u:filtered,u:logged &
+src/flowclassifier.py -i u:filtered,u:logged &
 /usr/bin/nemea/logger -i u:logged -t

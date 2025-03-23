33e947e4b4720a4a44945a335f762a732cb54e1c
Kakalasek <example@email.com>
2025-03-23 20:54:17 +0100

    start on interface

--- start_application.sh
+++ start_application.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+if [ -z $1 ]; then
+    echo "You have to provide an interface to listen to"
+fi
+
 cd src
 
-sudo ipfixprobe -i 'raw;ifc=ens3' -p "pstats" -p "tls" -o "unirec;i=u:mysocket:timeout=WAIT;p=(pstats,tls)" &
+sudo ipfixprobe -i "raw;ifc=$1" -p "pstats" -p "tls" -o "unirec;i=u:mysocket:timeout=WAIT;p=(pstats,tls)" &
 /usr/bin/nemea/unirecfilter -F 'port == 443' -i u:mysocket,u:filtered &
-src/flowclassifier.py -i u:filtered,u:logged &
+./flowclassifier.py -i u:filtered,u:logged &
 /usr/bin/nemea/logger -i u:logged -t

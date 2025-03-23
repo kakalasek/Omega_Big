3ec09f3550f28a156614ee1d668c8cb65e56dbe4
Kakalasek <example@email.com>
2025-03-23 16:31:30 +0100

    nothing

--- start_application.sh
+++ start_application.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-function cleanup {
-    exit 0
-}
-
-trap cleanup SIGINT
-trap cleanup SIGQUIT
-
 sudo ipfixprobe -i 'raw;ifc=ens3' -p "pstats" -p "tls" -o "unirec;i=u:mysocket:timeout=WAIT;p=(pstats,tls)" &
 /usr/bin/nemea/unirecfilter -F 'port == 443' -i u:mysocket,u:filtered &
 ./flow_classifier.py -i u:filtered,u:logged &
